3f36240c0906a7bf5f429e464c5687a009f2a11f
kalipy <3069087972@qq.com>
2022-05-09 17:42:20 +0800

    客户端搭建1

--- src/client/client.c
+++ src/client/client.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <getopt.h>
+
+#include "client.h"
+#include "../include/proto.h"
+
+struct client_conf_st client_conf = {
+    .rcvport = DEFAULT_RCVPORT,
+    .mgroup = DEFAULT_MGROUP,
+    .player_cmd = DEFAULT_PLAYERCMD
+}
+
+/*
+ * -M --mgroup 指定多播组
+ * -P --port   指定接收端口
+ * -p --player 指定播放器
+ * -H --help   显示帮助
+ */
+int main(int argc, char *argv[])
+{
+    /*
+     * 初始化级别：默认值，配置文件，环境变量，命令行参数
+     */
+    getopt_long();
+
+    socket();
+
+    pipe();
+
+    fork();
+
+    //紫禁城：调用解码器
+    //父进程：从网络上手包，发送给紫禁城
+
+    exit(0);
+}
--- src/client/client.h
+++ src/client/client.h
@@ -1,13 +1,15 @@
-/*
- * client.h
- * Copyright (C) 2022 kalipy <kalipy@debian>
- *
- * Distributed under terms of the MIT license.
- */
-
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#define DEFAULT_PLAYERCMD  "/usr/bin/mpg123 > /dev/null"
 
+struct client_conf_st
+{
+    char *rcvport;
+    char *mgroup;
+    char *player_cmd;
+};
+
+extern struct client_conf_st clent_conf;
 
 #endif /* !CLIENT_H */
