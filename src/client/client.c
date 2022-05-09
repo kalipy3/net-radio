27c2acddf10fe8f9c7a040f480c34567b7ce9b43
kalipy <3069087972@qq.com>
2022-05-09 17:59:54 +0800

    客户端搭建2

--- src/client/client.c
+++ src/client/client.c
@@ -16,6 +16,14 @@ struct client_conf_st client_conf = {
     .player_cmd = DEFAULT_PLAYERCMD
 }
 
+static void printhelp(void)
+{
+    printf("-P --port       指定接收端口\n
+            -M --mgroup     指定多播组\n
+            -p --player     指定播放器命令行\n 
+            -H --help       显示帮助\n");
+}
+
 /*
  * -M --mgroup 指定多播组
  * -P --port   指定接收端口
@@ -24,10 +32,39 @@ struct client_conf_st client_conf = {
  */
 int main(int argc, char *argv[])
 {
+    int c;
+    int index = 0;
+    struct option argarr[] = {{"prot", 1, NULL, 'P'}, {"mgroup", 1, NULL, 'M'}, {"player", 1, NULL, 'p'}, {"help", 0, NULL, 'H'}, {NULL, 0, NULL, 0}};
+
     /*
      * 初始化级别：默认值，配置文件，环境变量，命令行参数
      */
-    getopt_long();
+    while (1)
+    {
+        c = getopt_long(argc, argv, "P:M:p:H", argarr, &index);
+        if (c < 0)
+            break;
+
+        switch (c)
+        {
+            case 'P':
+                client_conf.rcvport = optarg;
+                break;
+            case 'M':
+                client_conf.mgroup = optarg;
+                break;
+            case 'p':
+                client_conf.player_cmd = optarg;
+                break;
+            case 'H':
+                printhelp();
+                exit(0);
+                break;
+            default:
+                abort();
+                break;
+        }
+    }
 
     socket();
 
