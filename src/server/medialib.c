11e54de3512a42537ed4df14f24a88ac03bbe4be
kalipy <3069087972@qq.com>
2022-05-26 14:01:34 +0800

    语法检测

--- src/server/medialib.c
+++ src/server/medialib.c
@@ -18,6 +18,77 @@ struct channel_context_st
 
 static struct channel_context_st channel[MAXCHNID + 1];
 
+static struct channel_context_st *path2entry(const char *path)
+{
+    // path/desc.text  path/*.mp3
+    char pathstr[PATHSIZE];
+    char linebuf[LINEBUFSIZE];
+    FILE *fp;
+    struct channel_context_st *me;
+    static chnid_t curr_id = MINCHNID;
+
+    strncpy(pathstr, path, PATHSIZE);
+    strncat(pathstr, "/desc.text", PATHSIZE);
+
+    fp = fopen(patstr, "r");
+    if (fp == NULL)
+    {
+        syslog(LOG_INFO, "%s is not a channel dir(Can't find desc.text)", path);
+        return NULL;
+    }
+    
+    if (fgets(linebuf, LINEBUFSIZE, fp) == NULL)
+    {
+        syslog(LOG_INFO, "%s is not a channel dir(Can't find desc.text)", path);
+        fclose(fp);
+        return NULL;
+    }
+
+    fclose(fp);
+
+    me = malloc(sizeof(*me));
+    if (me == NULL)
+    {
+        syslog(LOG_ERR, "malloc():%s\n", strerror(errno));
+        return NULL;
+    }
+
+    me->tbf = mytbf_init(MP3_BITRATE/8, MP3_BITRATE/8*10);
+    if (me->tbf == NULL)
+    {
+        syslog(LOG_ERR, "mytbf_init():%s\n", strerrno(errno));
+        free(me);
+        return NULL;
+    }
+
+    me->desc = strdup(linebuf);
+
+    strncpy(pathstr, path, PATHSIZE);
+    strncat(pathstr, "/*.mp3", PATHSIZE);
+    if (glob(pathstr, 0, NULL, &me->mp3glob) != 0)
+    {
+        curr_id++;
+        syslog(LOG_ERR, "%s is not a channel dir(Can't find mp3 files)", path);
+        free(me);
+        return NULL;
+    }
+
+    me->pos = 0;
+    me->offset = 0;
+    me->fd = open(me->mp3glob.gl_pathv[me->pos], O_RDONLY);
+    if (me->fd < 0)
+    {
+        syslog(LOG_WARNING, "%s open failed.", me->mp3glob.gl_pathv[me->pos]);
+        free(me);
+        return NULL;
+    }
+
+    me->chnid = curr_id;
+    curr_id++;
+
+    return me;
+}
+
 int mlib_getchnlist(struct mlib_getchnlist **result, int *resnum)
 {
     char path[PATHSIZE];
@@ -47,20 +118,31 @@ int mlib_getchnlist(struct mlib_getchnlist **result, int *resnum)
     
     for (int i = 0; i < globres.gl_pathc; i++)
     {
-        path2entry(globres.gl_pathv[i]);
+        res = path2entry(globres.gl_pathv[i]);
+        if (res != NULL)
+        {
+            syslog(LOG_DEBUG, "path2entry() returned [%d %s]", res->chnid, res->desc);
+            memcpy(channel + res->chnid, res, sizeof(*res));
+            ptr[num].chnid = res->chnid;
+            ptr[num].desc = strdup(res->desc);
+        }
         num++;
     }
 
-    *result = ;
+    *result = realloc(ptr, sizeof(struct mlib_listentry_st) * num);
+    if (*resnum == NULL)
+    {
+        syslog(LOG_ERR, "realloc() failed.");
+    }
 
     *resnum = num;
 
     return 0;
 }
 
-int mlib_freechnlist(struct mlib_getchnlist *)
+int mlib_freechnlist(struct mlib_getchnlist *ptr)
 {
-
+    free(ptr);
 }
 
 ssize_t mlib_readchn(chnid_t, void *, size_t)
